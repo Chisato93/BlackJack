268d30f6132393c0d7ab9b6f90cc6f5dc41810e2
kingofmess <151908469+Chisato93@users.noreply.github.com>
2024-06-04 22:39:02 +0900

    temp

--- Assets/Scripts/Seat/DealerSeat.cs
+++ Assets/Scripts/Seat/DealerSeat.cs
@@ -1,4 +1,11 @@
+using UnityEngine;
+
 public class DealerSeat : Seat
 {
+    public GameObject dummy_card;
 
+    public void SetActiveDummyCard(bool isActive)
+    {
+        dummy_card.SetActive(isActive);
+    }
 }
